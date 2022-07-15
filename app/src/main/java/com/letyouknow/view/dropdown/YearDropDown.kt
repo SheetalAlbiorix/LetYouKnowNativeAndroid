f5046a83a004f2b6d05807420a11fc3757cc4773
hardik.fullstack <hardik.fullstack@gmail.com>
2022-07-15 18:42:27 +0530

    -- initial commit

--- app/src/main/java/com/letyouknow/view/dropdown/YearDropDown.kt
+++ app/src/main/java/com/letyouknow/view/dropdown/YearDropDown.kt
@@ -1,41 +1,24 @@
 package com.letyouknow.view.dropdown
 
 import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.PopupWindow
+import com.letyouknow.R
 
 class YearDropDown(val context: Activity, val clickListener: View.OnClickListener) {
-    fun popupYear() {
-        val popupWindow = PopupWindow(context)
+    private fun showAlertFilter(v: View) {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.popup_dialog, null)
 
-        // the drop down list is a list view
+        val popup = PopupWindow(
+            view,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
 
-        // the drop down list is a list view
-        /* val listViewDogs = ListView(this)
-
-         // set our adapter and pass our pop up window contents
-
-         // set our adapter and pass our pop up window contents
-         listViewDogs.setAdapter(dogsAdapter(popUpContents))
-
-         // set the item click listener
-
-         // set the item click listener
-         listViewDogs.setOnItemClickListener(DogsDropdownOnItemClickListener())
-
-         // some other visual settings
-
-         // some other visual settings
-         popupWindow.isFocusable = true
-         popupWindow.width = 250
-         popupWindow.height = WindowManager.LayoutParams.WRAP_CONTENT
-
-         // set the list view as pop up window content
-
-         // set the list view as pop up window content
-         popupWindow.contentView = listViewDogs*/
-
-//        return popupWindow
+        return popup.showAsDropDown(v)
     }
-
 }
